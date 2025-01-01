349d943869a914ea06559331beabc5ffcb0ea0fe
nizamuddin0172 <neom01723441081@gmail.com>
2025-01-01 12:11:30 +0600

    add input fild

--- src/compnents/AddCoffee.jsx
+++ src/compnents/AddCoffee.jsx
@@ -3,11 +3,18 @@ const AddCoffee = () => {
     <div>
       <h2 className="text-center text-3xl">Add Coffee</h2>
       <form>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered input-secondary w-full max-w-xs"
-        />
+        <div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Coffee Name"
+              className="input input-bordered input-accent w-full max-w-xs"
+            />
+          </div>
+        </div>
       </form>
     </div>
   );
